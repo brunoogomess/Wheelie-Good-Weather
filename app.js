7f4578615f98d45d0f63b07d385724fc3adcafcd
Bruno Gomes <brunomgprofissional@gmail.com>
2026-02-07 15:16:21 +0000

    Added weather code

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const credentials = require("./credentials.json");
 
 const url_list = [
-  (lat, lon, key) => `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,precipitation,precipitation_probability&forecast_days=1`,
+  (lat, lon, key) => `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&hourly=temperature_2m,precipitation,precipitation_probability,weather_code&forecast_days=1`,
   //api url for openweathermap
   //(lat, lon, key) => `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${key}`
 ];
@@ -12,14 +12,17 @@ async function call_api(lat, lon) {
     try {
       const res = await fetch(endpoint);
       const weatherForecast = await res.json()
+      console.log(weatherForecast)
       let hourOfTheDay = weatherForecast.hourly.time
       let hourlyTemp = weatherForecast.hourly.temperature_2m
       let hourlyPrecipitationProbability = weatherForecast.hourly.precipitation_probability
+      let hourlyWeatherCode = weatherForecast.hourly.weather_code
       const weatherSummary = hourOfTheDay.map((hour, index) => {
         return {
           time: hour,
           temp: hourlyTemp[index],
-          precip: hourlyPrecipitationProbability[index]
+          precip: hourlyPrecipitationProbability[index],
+          weathercode: hourlyWeatherCode[index]
         };
       });
       console.log(weatherSummary)
